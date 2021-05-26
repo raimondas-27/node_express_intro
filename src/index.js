76580c6a7679ff64f5ac3dec6fff7d506c1a8420
raimondas-27 <raimondasvilnius@yahoo.com>
2021-05-26 13:46:56 +0300

    created people.js inported and exported people object

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
 const express = require("express");
 const path = require("path");
+const {people } = require("./js/people");
 
 const app = express();
+
+//routes
+const routePath = path.join(__dirname, "html");
 const indexHtmlPath = path.join(__dirname, "html/index.html");
 const aboutHtmlPath = path.join(__dirname, "html/about.html");
 
 
-app.get("/home", (request, response) => response.sendFile(indexHtmlPath));
+// app.get("/home", (request, response) => response.sendFile(indexHtmlPath));
+// app.get("/about", (request, response) => response.sendFile(aboutHtmlPath));
 
-app.get("/about", (request, response) => response.sendFile(aboutHtmlPath));
+
+//kai turime papke kurios failus norime pasiekti, is narsykles pagal pavadinimus, nustatome
+//static folderi
+
+app.use(express.static(routePath));
 
 
 app.listen(4000, () => console.log("server is running"));
