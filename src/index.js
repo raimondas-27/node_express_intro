4bf635d274f1a3521b508894371280556fb16b8e
raimondas-27 <raimondasvilnius@yahoo.com>
2021-05-26 13:25:11 +0300

    created index.html, also created a path to index html

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 const express = require("express");
+const path = require("path");
+
 const app = express();
+const indexHtmlPath = path.join(__dirname, "html/index.html");
+console.log(indexHtmlPath)
 
 app.get("/", (request, response) => {
-   response.send("<h1> Hello world from express</h1>")
-})
+   // response.send("<h1> Hello world from express</h1>")
+   response.sendFile(indexHtmlPath);
+});
+
 
 app.listen(4000, () => console.log("server is running"));
 
