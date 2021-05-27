9107b882e811e66f18ea534a755047e765d0f51d
raimondas-27 <raimondasvilnius@yahoo.com>
2021-05-27 11:33:11 +0300

    update put request created and works

--- server/routes/api/peopleApi.js
+++ server/routes/api/peopleApi.js
@@ -1,26 +1,28 @@
 const express = require('express');
 const router = express.Router();
 let personId = 6;
-
 const people = require('../../js/peopleData');
+
 
 // get all people Endpoint
 router.get('/', (req, res) => {
-  // grazinam json
-  res.json(people);
+   // grazinam json
+   res.json(people);
 });
+
+
 // get one people Endpoint
 router.get('/:id', (req, res) => {
-  const paramId = req.params.id;
+   const paramId = req.params.id;
 
-  const found = people.find((p) => p.id === paramId);
+   const found = people.find((p) => p.id === paramId);
 
-  if (!found) {
-    res.status(404).json({ errorMsg: `sorry person with id ${paramId} was not found` });
-  }
+   if (!found) {
+      res.status(404).json({errorMsg: `sorry person with id ${paramId} was not found`});
+   }
 
-  // grazinam json
-  res.json(found);
+   // grazinam json
+   res.json(found);
 });
 
 // Create one people Endpoint
@@ -29,13 +31,31 @@ router.post('/', (req, res) => {
    console.log(' req.body', req.body);
 
    const newPerson = {
-      id : (++personId).toString(),
+      id: (++personId).toString(),
       name: req.body.name,
       surname: req.body.surname,
    }
    people.push(newPerson)
 
    res.json(people);
+});
+
+
+// edit one people Endpoint
+router.put('/:id', (req, res) => {
+   console.log("executing put request")
+   const paramId = req.params.id;
+
+   const found = people.find((p) => p.id === paramId);
+
+   if (!found) {
+      res.status(404).json({errorMsg: `sorry person with id ${paramId} was not found`});
+   }
+
+   // atnaujinti zmogaus duomenis
+   found.name = req.body.name;
+   found.surname = req.body.surname;
+   res.json({msg : "user was updated", updatedUser : found})
 });
 
 module.exports = router;
